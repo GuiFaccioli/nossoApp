cac0ad83084cf5e5df76828b1f097d6615b09e4a
Guilherme Faccioli <guifacciolic@gmail.com>
2026-01-19 17:41:25 -0300

    test

---
+++ js/nossoApp.js
@@ -0,0 +1,40 @@
+function roscaDireta() {
+
+    document.getElementById("roscaDireta").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+function roscaAlternada() {
+
+    document.getElementById("roscaAlternada").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+function roscaMartelo() {
+
+    document.getElementById("roscaMartelo").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+function roscaConcentrada() {
+
+    document.getElementById("roscaConcentrada").scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+
+function rosca21() {
+
+    document.getElementById("rosca21").scrollIntoView({
+        behavior:"smooth",
+        block:"start"
+    });
+}
